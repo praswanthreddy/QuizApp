cfd73c93628b74e992239d43eb8b2e66a1d5c26e
Praswanth <praswanth@gmail.com>
2022-09-06 20:21:35 +0530

    second commit

--- quizApp.js
+++ quizApp.js
@@ -4,36 +4,46 @@ export default class QuizApp extends LightningElement {
     myQuestions=[
         {
             id:"Question1",
-            question:"Which of the following is not a template loop?",
+            question:"In Which tag we can use directive in HTML file ?",
             answers:{
-                a:"for :each",
-                b:"iterator",
-                c:"map loop",
+                a:"div",
+                b:"form",
+                c:"template",
             },
             correctAnswer:"c"
         },
         {
             id:"Question2",
-            question:"Which of the following is invalid in LWC component folder?",
+            question:"Which of the following file is not a default file in LWC component folder?",
             answers:{
-                a:".svg",
-                b:".apex",
-                c:".js",
+                a:"xml",
+                b:"apex",
+                c:"js",
             },
             correctAnswer:"b"
         },
         {
             id:"Question3",
-            question:"Which of the following is not a directive?",
+            question:"Which of the following is not a array method?",
             answers:{
-                a:"if:true",
-                b:"@track",
-                c:"if:false",
+                a:"filter",
+                b:"length",
+                c:"map",
             },
             correctAnswer:"b"
         }
     ]
     selected={}//for storing answers
+    isSubmitted=false
+    correctAnswers=0
+
+    get allNotSelected(){
+        return !(Object.keys(this.selected).length===this.myQuestions.length)
+    }
+
+    get isScoredFull(){
+        return `slds-text-heading_large ${this.myQuestions.length === this.correctAnswers ? 'slds-text-color_success':'slds-text-color_error'}`
+    }
 
     //change handler method
     changeHandler(event){
@@ -43,12 +53,19 @@ export default class QuizApp extends LightningElement {
         this.selected={...this.selected,[name]:value}
     }
 
-    submitHandler(){
+    submitHandler(event){
+        event.preventDefault()
+        let correct=this.myQuestions.filter(item=>this.selected[item.id]===item.correctAnswer)
+        this.correctAnswers= correct.length
+        this.isSubmitted=true
+        console.log("this.correctAnswers",this.correctAnswers)
 
     }
 
     resetHandler(){
-        
+        this.selected={}
+        this.correctAnswers=0
+        this.isSubmitted=false
     }
 
 }
